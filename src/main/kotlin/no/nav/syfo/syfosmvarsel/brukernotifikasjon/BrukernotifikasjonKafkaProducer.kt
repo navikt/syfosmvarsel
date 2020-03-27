6612998e3af2998f0d69bd63fc2fccc939227925
Tia Firing <tia.firing@nav.no>
2020-03-27 10:20:06 +0100

    mer logg for debugging

--- src/main/kotlin/no/nav/syfo/syfosmvarsel/brukernotifikasjon/BrukernotifikasjonKafkaProducer.kt
+++ src/main/kotlin/no/nav/syfo/syfosmvarsel/brukernotifikasjon/BrukernotifikasjonKafkaProducer.kt
@@ -3,6 +3,7 @@ package no.nav.syfo.syfosmvarsel.brukernotifikasjon
 import no.nav.brukernotifikasjon.schemas.Done
 import no.nav.brukernotifikasjon.schemas.Nokkel
 import no.nav.brukernotifikasjon.schemas.Oppgave
+import no.nav.syfo.syfosmvarsel.log
 import org.apache.kafka.clients.producer.KafkaProducer
 import org.apache.kafka.clients.producer.ProducerRecord
 
@@ -13,10 +14,13 @@ class BrukernotifikasjonKafkaProducer(
     private val brukernotifikasjonDoneTopic: String
 ) {
     fun sendOpprettmelding(nokkel: Nokkel, oppgave: Oppgave) {
+        log.info("Logger nøkkelinfo: systembruker={}, grupperingsid={}", nokkel.getSystembruker(), nokkel.getEventId())
         kafkaproducerOpprett.send(ProducerRecord(brukernotifikasjonOpprettTopic, nokkel, oppgave))
     }
 
     fun sendDonemelding(nokkel: Nokkel, done: Done) {
+        log.info("Logger nøkkelinfo: systembruker={}, grupperingsid={}", nokkel.getSystembruker(), nokkel.getEventId())
+        log.info("Logger done: fnr={}, grupperingsid = {}", done.getFodselsnummer(), done.getGrupperingsId())
         kafkaproducerDone.send(ProducerRecord(brukernotifikasjonDoneTopic, nokkel, done))
     }
 }
