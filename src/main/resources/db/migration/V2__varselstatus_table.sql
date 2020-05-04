705b967775523953ccee229830bda4e4576c0555
Tia Firing <tia.firing@nav.no>
2020-05-04 11:55:20 +0200

    ny tabell for varselbestilling

---
+++ src/main/resources/db/migration/V2__varselstatus_table.sql
@@ -0,0 +1,9 @@
+CREATE TABLE varselstatus (
+    id                      INT             GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+    sykmelding_id           uuid            NOT NULL,
+    opprettet               timestamptz     NOT NULL,
+    mottaker_fnr            VARCHAR(64)     NOT NULL,
+    varselbestilling_id     uuid            NOT NULL
+);
+
+create index varselstatus_sykmelding_id_idx on varselstatus (sykmelding_id);
