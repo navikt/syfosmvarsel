b2293e7597ba5ceb02b2f10cbaf706333b9f6f55
Tia Firing <tia.firing@nav.no>
2022-12-16 14:32:43 +0100

    oppgraderinger

--- build.gradle.kts
+++ build.gradle.kts
@@ -7,26 +7,27 @@ version = "1.0.0"
 
 val coroutinesVersion = "1.6.4"
 val kluentVersion = "1.72"
-val ktorVersion = "2.1.3"
-val logbackVersion = "1.4.4"
+val ktorVersion = "2.2.1"
+val logbackVersion = "1.4.5"
 val prometheusVersion = "0.16.0"
 val kotestVersion = "5.5.4"
 val logstashEncoderVersion = "7.2"
 val kafkaVersion = "3.3.1"
-val jacksonVersion = "2.14.0"
-val smCommonVersion = "1.966f3d4"
+val jacksonVersion = "2.14.1"
+val smCommonVersion = "1.1490275"
 val avroVersion = "1.11.0"
 val confluentVersion = "7.2.1"
-val postgresVersion = "42.5.0"
-val flywayVersion = "9.8.1"
+val postgresVersion = "42.5.1"
+val flywayVersion = "9.8.3"
 val hikariVersion = "5.0.1"
 val brukernotifikasjonAvroVersion = "1.2022.04.13-12.09-292ce6d359bd"
 val mockkVersion = "1.13.2"
-val kotlinVersion = "1.7.21"
-val testContainerVersion = "1.17.4"
+val kotlinVersion = "1.7.22"
+val testContainerVersion = "1.17.6"
+val nettyCodecVersion = "4.1.86.Final"
 
 plugins {
-    id("org.jetbrains.kotlin.jvm") version "1.7.21"
+    id("org.jetbrains.kotlin.jvm") version "1.7.22"
     id("org.jmailen.kotlinter") version "3.12.0"
     id("com.diffplug.spotless") version "6.11.0"
     id("com.github.johnrengelman.shadow") version "7.1.2"
@@ -55,6 +56,9 @@ dependencies {
 
     implementation("io.ktor:ktor-server-core:$ktorVersion")
     implementation("io.ktor:ktor-server-netty:$ktorVersion")
+    // This is to override version that is in io.ktor:ktor-server-netty
+    // https://www.cve.org/CVERecord?id=CVE-2022-41915
+    implementation("io.netty:netty-codec:$nettyCodecVersion")
     implementation("io.ktor:ktor-serialization-jackson:$ktorVersion")
 
     implementation("io.prometheus:simpleclient_hotspot:$prometheusVersion")
