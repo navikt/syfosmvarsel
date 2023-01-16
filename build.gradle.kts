cc80050e3e6005d2265fa36cda0a7222852a055d
Joakim Taule Kartveit <joakimkartveit@gmail.com>
2023-01-16 12:13:28 +0100

    Upgraded ktor to 2.2.2
    Upgraded syfosm-common
    Upgraded kotlin to 1.8.0

--- build.gradle.kts
+++ build.gradle.kts
@@ -7,14 +7,14 @@ version = "1.0.0"
 
 val coroutinesVersion = "1.6.4"
 val kluentVersion = "1.72"
-val ktorVersion = "2.2.1"
+val ktorVersion = "2.2.2"
 val logbackVersion = "1.4.5"
 val prometheusVersion = "0.16.0"
 val kotestVersion = "5.5.4"
 val logstashEncoderVersion = "7.2"
 val kafkaVersion = "3.3.1"
 val jacksonVersion = "2.14.1"
-val smCommonVersion = "1.1490275"
+val smCommonVersion = "1.d6548c5"
 val avroVersion = "1.11.0"
 val confluentVersion = "7.2.1"
 val postgresVersion = "42.5.1"
@@ -22,12 +22,11 @@ val flywayVersion = "9.8.3"
 val hikariVersion = "5.0.1"
 val brukernotifikasjonAvroVersion = "1.2022.04.13-12.09-292ce6d359bd"
 val mockkVersion = "1.13.2"
-val kotlinVersion = "1.7.22"
+val kotlinVersion = "1.8.0"
 val testContainerVersion = "1.17.6"
-val nettyCodecVersion = "4.1.86.Final"
 
 plugins {
-    id("org.jetbrains.kotlin.jvm") version "1.7.22"
+    id("org.jetbrains.kotlin.jvm") version "1.8.0"
     id("org.jmailen.kotlinter") version "3.12.0"
     id("com.diffplug.spotless") version "6.11.0"
     id("com.github.johnrengelman.shadow") version "7.1.2"
@@ -56,9 +55,6 @@ dependencies {
 
     implementation("io.ktor:ktor-server-core:$ktorVersion")
     implementation("io.ktor:ktor-server-netty:$ktorVersion")
-    // This is to override version that is in io.ktor:ktor-server-netty
-    // https://www.cve.org/CVERecord?id=CVE-2022-41915
-    implementation("io.netty:netty-codec:$nettyCodecVersion")
     implementation("io.ktor:ktor-serialization-jackson:$ktorVersion")
 
     implementation("io.prometheus:simpleclient_hotspot:$prometheusVersion")
